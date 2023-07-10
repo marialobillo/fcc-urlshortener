ef7e26c83ba066db0fe8ebc47085c4209d510dc8
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-07-10 16:49:39 +0200

    fix: add invalid url message

--- index.js
+++ index.js
@@ -30,33 +30,6 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// app.post('/api/shorturl', (req, res) => {
-//   console.log('The body is: ', req.body)
-//   const url = new URL(req.body.url);
-//   const hostnameURL = url.hostname;
-//   dns.lookup(
-//     hostnameURL, 
-//     async (error, address, family) => {
-//       if (error) {
-//         console.error(error);
-//         return res.status(400).json({ error: 'invalid url' });
-//       } else {
-//         // if no error exists, we save the url to the database
-//         console.log(
-//           `The ip address is ${address} and the ip version is ${family}`
-//         );
-//         return res.status(200).json({ message: 'valid url' });
-//       }
-//   })
-// })
-
-// app.get('/app/shorturl/:short_url', (req, res) => {
-//   const short_url = req.params.short_url;
-//   console.log('The short url is: ', short_url);
-//   res.status(200).json({ message: 'short url' });
-// })
-
-
 
 // Connect to the database and start the server
 const start = async () => {
--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -21,7 +21,7 @@ const createShortUrl = async (req, res) => {
                 } 
             })
     } catch (error) {  
-        res.status(500).json({ error })
+        res.status(500).json({ error: 'invalid url' })
     }
 }
 
